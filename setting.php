10705c6f9ab998ae2dc2a83f0192307d11581088
Hamzbsk <55208456+Hamzbsk@users.noreply.github.com>
2019-09-12 03:43:43 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -1,5 +1,5 @@
 <?php
-$warn=$pth."[{$red}!{$pth}]{$red} WARNING!!\n{$kn}[{$red}!{$kn}] this is an illegal program !!\n{$kn}[{$red}!{$kn}] the risk is entirely borne by the user !!{$pth}".$t;
+$warn=$kn."[{$red}!{$kn}]{$red} WARNING!!\n{$pth}[{$red}!{$pth}] this is an illegal program !!\n{$pth}[{$red}!{$pth}] the risk is entirely borne by the user !!{$pth}".$t;
 
 $big="
 {$tr}██████{$pth}╗{$tr} ██{$pth}╗{$tr} ██████{$pth}╗{$tr}████████{$pth}╗{$tr} ██████{$pth}╗{$tr} ██{$pth}╗{$tr}  ██{$pth}╗{$tr}███████{$pth}╗{$tr}███{$pth}╗{$tr}   ██{$pth}╗{$tr}
@@ -9,8 +9,8 @@ $big="
 ██████{$pth}╔╝{$tr}██{$pth}║╚{$tr}██████{$pth}╔╝{$tr}  ██{$pth}║   ╚{$tr}██████{$pth}╔╝{$tr}██{$pth}║{$tr}  ██{$pth}╗{$tr}███████{$pth}╗{$tr}██{$pth}║ ╚{$tr}████{$pth}║
 ╚═════╝ ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝
 [{$ijo}*{$pth}] Created :{$ijo} Cocentz ⭐⭐⭐ {$pth}[{$ijo}*{$pth}] Versions:{$red} v.2.0.1{$tr} Ready to use{$pth}
-[{$ijo}*{$pth}] Youtube :{$red} Cocentz 404{$pth}    [{$ijo}^{$pth}] Support :{$tr} AW 2000{$pth} &{$br} MK KHAIRIL{$pth}
-[{$ijo}*{$pth}] Contact :{$ijo} +6285218xxxxxx{$pth} [{$red}!{$pth}]{$kn} Please subscribe to my Youtube{$pth}
-[{$ijo}*{$pth}] Telegram:{$tr} script_termux{$ht}  [#] Copyright © - 2019 ({$ijo}Cocentz{$ht}){$pth}
+[{$ijo}*{$pth}] Youtube :{$red} Cocentz 404{$pth}    [{$ijo}*{$pth}] Support :{$tr} AW 2000{$pth} &{$br} MK KHAIRIL{$pth}
+[{$ijo}*{$pth}] Contact :{$ijo} +6285218xxxxxx{$pth} [{$red}*{$pth}]{$kn} Please subscribe to my Youtube{$pth}
+[{$ijo}*{$pth}] Telegram:{$tr} script_termux{$ht}  [*] Copyright © - 2019 ({$ijo}Cocentz{$ht}){$pth}
 ═══════════════════════════════════════════════════════════════".$t;
 $stat_big=true;
