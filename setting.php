e3a7447126e0ce879a2db129f9b194c3bc121a4e
Hamzbsk <55208456+Hamzbsk@users.noreply.github.com>
2019-09-12 03:49:17 +0700

    Update setting.php

--- setting.php
+++ setting.php
@@ -8,9 +8,7 @@ $big="
 ██{$pth}╔══{$tr}██{$pth}╗{$tr}██{$pth}║{$tr}██{$pth}║{$tr}   ██{$pth}║{$tr}  ██{$pth}║{$tr}   ██{$pth}║{$tr}   ██{$pth}║{$tr}██{$pth}╔═{$tr}██{$pth}╗{$tr} ██{$pth}╔══╝{$tr}  ██{$pth}║╚{$tr}██{$pth}╗{$tr}██{$pth}║{$tr}
 ██████{$pth}╔╝{$tr}██{$pth}║╚{$tr}██████{$pth}╔╝{$tr}  ██{$pth}║   ╚{$tr}██████{$pth}╔╝{$tr}██{$pth}║{$tr}  ██{$pth}╗{$tr}███████{$pth}╗{$tr}██{$pth}║ ╚{$tr}████{$pth}║
 ╚═════╝ ╚═╝ ╚═════╝   ╚═╝    ╚═════╝ ╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝
-[{$ijo}*{$pth}] Created :{$ijo} Cocentz ⭐⭐⭐ {$pth}[{$ijo}*{$pth}] Versions:{$red} v.2.0.1{$tr} Ready to use{$pth}
-[{$ijo}*{$pth}] Youtube :{$red} Cocentz 404{$pth}    [{$ijo}*{$pth}] Support :{$tr} AW 2000{$pth} &{$br} MK KHAIRIL{$pth}
-[{$ijo}*{$pth}] Contact :{$ijo} +6285218xxxxxx{$pth} [{$red}*{$pth}]{$kn} Please subscribe to my Youtube{$pth}
-[{$ijo}*{$pth}] Telegram:{$tr} script_termux{$ht}  [*] Copyright © - 2019 ({$ijo}Cocentz{$ht}){$pth}
+[{$ijo}*{$pth}] Created :{$ijo} TN.Cocentz {$pth} [{$ijo}*{$pth}] Versions:{$red} v.2.0.1{$pth}
+[{$ijo}*{$pth}] Youtube :{$red} Cocentz 404{$pth} [{$ijo}*{$pth}] Support :{$tr} AW 2000{$pth} &{$br} MK KHAIRIL{$pth}
 ═══════════════════════════════════════════════════════════════".$t;
 $stat_big=true;
